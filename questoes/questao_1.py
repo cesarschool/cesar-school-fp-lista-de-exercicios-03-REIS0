a7b295e507bde5a487f5f9cd339bffcf2ae52305
REIS0 <aor@cesar.school>
2018-05-17 19:43:10 -0300

    Update questao_1.py
    teste

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -28,7 +28,32 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 1")
+    usuario = str(input("Digite um nome de usuário: "))
+    logado = 0
+    isalpha = 0
+    isnumeric = 0
+    isupper = 0
+    desisto = 0
+    while logado == 0:
+        senha = str(input("1. Pelo menos uma letra entre [a-z]\n2. Pelo menos 1 número entre [0-9]\n3. Pelo menos uma letra entre [A-Z]\n4. Pelo menos 1 caractere de [$ # @]\n5. Comprimento mínimo da senha: 6\n6. Comprimento máximo da senha: 12\nDigite uma senha: "))
+        if senha.isalnum() == True:
+            for char in senha:
+                if char.isalpha() == True:
+                    isalpha = 1
+                    pass
+                if char.isnumeric() == True:
+                    isnumeric = 1
+                    pass
+                if char.isupper() == True:
+                    isupper = 1
+                    pass
+            if isalpha == 1 and isnumeric == 1 and isupper == 1:
+                logado = 1
+                desisto = 1
+                print('senha válida\nsua senha é:',senha)
+                break
+            elif desisto == 0:
+                print('senha inválida')
     
 
 
